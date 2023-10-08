a2e9917898c340342d8aea7a3b86909ce61dce92
ER RAGRAGUI HATIM <hatim.er@protonmail.com>
2023-10-08 23:34:39 +0200

    init

---
+++ App.js
@@ -0,0 +1,11 @@
+import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <SafeAreaInsetsContext.Consumer>
+        {(insets) => <View style={{ paddingTop: insets.top }} />}
+      </SafeAreaInsetsContext.Consumer>
+    );
+  }
+}
